ab3b7a090f8bc368172daeeab1d183a1345acbc2
therealJimoh <jimoh.abdullah69@gmail.com>
2022-02-12 11:47:13 +0100

    initial commit

--- src/containers/paymentinfo/PaymentInfo.jsx
+++ src/containers/paymentinfo/PaymentInfo.jsx
@@ -28,60 +28,81 @@ class PaymentInfo extends Component {
   render() {
     return (
       <>
-        <div className="nck__paymentinfo-cards">
-          <Cards
-            cvc={this.state.cvc}
-            expiry={this.state.expiry}
-            focused={this.state.focus}
-            name={this.state.name}
-            number={this.state.number}
-          />
-        </div>
-        <div className="nck__paymentinfo-form">
-          <form>
-            <div className="nck__paymentinfo-form-wrapper">
-              <div className="nck__paymentinfo-form-container">
-                <label htmlFor="Credit Card Number"></label>
-                <input
-                  type="tel"
-                  name="number"
-                  placeholder="Card Number"
-                  onChange={this.handleInputChange}
-                  onFocus={this.handleInputFocus}
-                />
+        <div className="nck__paymentinfo">
+          <div className="nck__paymentinfo-cards">
+            <Cards
+              cvc={this.state.cvc}
+              expiry={this.state.expiry}
+              focused={this.state.focus}
+              name={this.state.name}
+              number={this.state.number}
+            />
+          </div>
+          <div className="nck__paymentinfo-form">
+            <form>
+              <div className="nck__paymentinfo-form-wrapper">
+                <div className="nck__payment-form-inputs">
+                  <div className="nck__paymentinfo-input-flex1">
+                    <div className="nck__paymentinfo-form-container">
+                      <label htmlFor="Credit Card Number">
+                        Credit card number
+                      </label>
+                      <input
+                        type="tel"
+                        name="number"
+                        maxlength="16"
+                        placeholder="Card Number"
+                        onChange={this.handleInputChange}
+                        onFocus={this.handleInputFocus}
+                      />
+                    </div>
+                    <div className="nck__paymentinfo-form-container">
+                      <label htmlFor="Expiration date">Expiration date</label>
+                      <input
+                        type="tel"
+                        name="expiry"
+                        placeholder="Expiry date"
+                        onChange={this.handleInputChange}
+                        onFocus={this.handleInputFocus}
+                      />
+                    </div>
+                  </div>
+                  <div className="nck__paymentinfo-input-flex2">
+                    <div className="nck__paymentinfo-form-container">
+                      <label htmlFor="Security code">Security code</label>
+                      <input
+                        type="tel"
+                        name="cvc"
+                        placeholder="Security code"
+                        onChange={this.handleInputChange}
+                        onFocus={this.handleInputFocus}
+                      />
+                    </div>
+                    <div className="nck__paymentinfo-form-container">
+                      <label htmlFor="Postal Code">Postal code</label>
+                      <input
+                        type="tel"
+                        name="postal code"
+                        placeholder="Postal code"
+                        onChange={this.handleInputChange}
+                        onFocus={this.handleInputFocus}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="nck__paymentinfo-checkbox">
+                  <label className="nck__paymentinfo-checkbox-container">
+                    <input type="radio" checked="checked" name="radio" />
+                    <span className="nck__paymentinfo__checkbox-checkmark"></span>
+                  </label>
+                  <p> Use this card for next purchase </p>
+                </div>
+                <div className="nck__paymentinfo-form-submit">
+                  <input type="submit" value="Add Card" />
+                </div>
               </div>
-              <div className="nck__paymentinfo-form-container">
-                <label htmlFor="Expiration date"></label>
-                <input
-                  type="tel"
-                  name="expiry"
-                  placeholder="Expiry date"
-                  onChange={this.handleInputChange}
-                  onFocus={this.handleInputFocus}
-                />
-              </div>
-              <div className="nck__paymentinfo-form-container">
-                <label htmlFor="Security code"></label>
-                <input
-                  type="tel"
-                  name="cvc"
-                  placeholder="Security code"
-                  onChange={this.handleInputChange}
-                  onFocus={this.handleInputFocus}
-                />
-              </div>
-              <div className="nck__paymentinfo-form-container">
-                <label htmlFor="Postal Code"></label>
-                <input
-                  type="tel"
-                  name="postal code"
-                  placeholder="Postal code"
-                  onChange={this.handleInputChange}
-                  onFocus={this.handleInputFocus}
-                />
-              </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       </>
     );
